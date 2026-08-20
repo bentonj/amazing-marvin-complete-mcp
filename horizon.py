e2c9ae92940112f3a1b66ce2428e3051df30a3b5
bentonj <bentonj@gmail.com>
2026-08-20 16:32:34 -0700

    fixing marvin_mcp load issue...

--- horizon.py
+++ horizon.py
@@ -1,1 +1,6 @@
-from marvin_mcp.server import mcp
+from marvin_mcp import server
+from marvin_mcp.config import load_settings
+
+def create_server():
+    server.init(load_settings())
+    return server.mcp
